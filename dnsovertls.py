0dc65efdb37aed5b6bfd94e66e52018f6ab4a724
Balamurugan <47313756+bala280597@users.noreply.github.com>
2022-01-27 00:13:46 +0530

    Update dnsovertls.py

--- dnsovertls.py
+++ dnsovertls.py
@@ -3,13 +3,14 @@ import ssl
 import sys
 
 def send_query(tsock,dns_query):
+  # Send and Receive data
   tsock.send(dns_query)
   result=tsock.recv(1024)
   tsock.close()
   return result
 
 def tls_connection(dns,port):
-
+  # TLS Wrapping 
   sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   sock.settimeout(10)
   context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
@@ -28,6 +29,7 @@ def new_request(data,address,dns,port):
     rcode = tcp_result[:6].encode("hex")
     # Get 12th bit from the response
     rcode = str(rcode)[11:]
+    # Check 12 th bit with condition
     if (rcode != '0'):
       sys.exit('Error RCODE', rcode)
     else:
@@ -37,17 +39,20 @@ def new_request(data,address,dns,port):
 
 if __name__ == '__main__':
   HEADER = 1024
+  # Proxy server and Port
   port = 12555
   host='0.0.0.0'
+  # DNS server and Port
   dns_server = '1.1.1.1'
   dns_port   = 853
   ADDR = (host, port)
-
   try:
+    # Socket Connection and Binding
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind(ADDR)
     s.listen(10)
     while True:
+      # Receive DNS query from client
       conn, addr = s.accept()
       data = conn.recv(HEADER)
       result = new_request(data, addr, dns_server, dns_port)
