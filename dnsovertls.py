0155bd1919d0307a4b4ebd09cf44fe38e7d148cf
Balamurugan <47313756+bala280597@users.noreply.github.com>
2022-01-26 23:59:44 +0530

    ADD: python file

---
+++ dnsovertls.py
@@ -0,0 +1,56 @@
+import socket
+import ssl
+import sys
+
+def send_query(tsock,dns_query):
+  tsock.send(dns_query)
+  result=tsock.recv(1024)
+  tsock.close()
+  return result
+
+def tls_connection(dns,port):
+
+  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  sock.settimeout(10)
+  context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+  context.verify_mode = ssl.CERT_REQUIRED
+  context.load_verify_locations('/etc/ssl/certs/ca-certificates.crt')
+  wrappedSocket = context.wrap_socket(sock, server_hostname=dns)
+  wrappedSocket.connect((dns , int(port)))
+  return wrappedSocket
+
+# send new request to upstream dns server and return the result
+def new_request(data,address,dns,port):
+  tsock=tls_connection(dns,port)
+  tcp_result = send_query(tsock, data)
+  if tcp_result:
+    # Get first 6 bytes from response
+    rcode = tcp_result[:6].encode("hex")
+    # Get 12th bit from the response
+    rcode = str(rcode)[11:]
+    if (rcode != '0'):
+      sys.exit('Error RCODE', rcode)
+    else:
+      return tcp_result
+  else:
+    sys.exit('Error occurred')
+
+if __name__ == '__main__':
+  HEADER = 1024
+  port = 12555
+  host='0.0.0.0'
+  dns_server = '1.1.1.1'
+  dns_port   = 853
+  ADDR = (host, port)
+
+  try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(ADDR)
+    s.listen(10)
+    while True:
+      conn, addr = s.accept()
+      data = conn.recv(HEADER)
+      result = new_request(data, addr, dns_server, dns_port)
+      conn.sendto(result, addr)
+  except:
+    s.close()
